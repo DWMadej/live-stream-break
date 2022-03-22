7b680e3e5c540fc2b603b09dd43b66ebf6e715a6
Daniel Madej <SA07DM@sams.ac.uk>
2022-03-22 08:56:56 +0000

    Added credentials file

--- renderer.js
+++ renderer.js
@@ -17,7 +17,8 @@ document.getElementById('spreadsheetIDRefresh').addEventListener('click', functi
 
   extractSheets({
     spreadsheetKey: spreadsheetID,
-    credentials: 'AIzaSyA9xkY5mOF3a_5kFm26vtUMpcjuhDrTpgo'
+    // credentials: 'AIzaSyA9xkY5mOF3a_5kFm26vtUMpcjuhDrTpgo'
+    credentials: require("./google-creds.json").key
     // other options...
   })
     .then(({ Sheet1 }) => {
